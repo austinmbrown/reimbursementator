c91ee6cb45de6c3b3999553730b6ceb300c6088f
Austin Brown <austinmbrown@gmail.com>
2023-01-16 16:08:56 -0500

    starting quick and dirty, not liking where its going

--- reimburse.rb
+++ reimburse.rb
@@ -1,4 +1,5 @@
 puts "hello world"
+require 'date'
 
 SET_1 = [
   {
@@ -76,11 +77,24 @@ SET_4 = [
 # find start and end dates for set
 # iterate through dates, build array of "full" or "travel" elements
 
-class Reimburser do 
+class Reimburser  
   
-  def calculate(projects)
-    
-
+  def calculate(projects) 
+    set_start_date = projects.map{|p| Date.parse(p[:start_date])}.min
+    set_end_date = projects.map{|p| Date.parse(p[:end_date])}.max
+    set_range = (set_start_date..set_end_date)
+    days = []
+    set_range.each_with_index do |d, i| 
+      # First day and last day of a project, or sequence of projects, is a travel day.
+      if i == 0 || i == set_range.to_a.length - 1
+        days << :travel
+        next
+      end
+      
+      #Any day in the middle of a project, or sequence of projects, is considered a full day.
+      days << :full
+    end
+    return days
   end
 
 end
